791dbd712394f83c2f96295ada922cc8510e5e4f
salisspro <salisspro@email.com>
2024-12-24 01:24:56 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-mono'>icons test</h1>
+
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+// import { FaRegHeart } from "react-icons/fa"
+// import { MdHomeFilled } from "react-icons/md";
+// import { IoBookmarkOutline } from "react-icons/io5";
+// import { FaArrowRight } from "react-icons/fa6";
+// import { HiOutlineExclamationCircle } from "react-icons/hi"
+
+
+{/* <FaRegHeart />
+<MdHomeFilled  className='text-3xl'/>
+<IoBookmarkOutline />
+<FaArrowRight />
+<HiOutlineExclamationCircle /> */}
